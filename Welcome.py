a60c38f6e2dea3131dfa132aaf25e625f2fbe380
mzoelln <138578608+mzoelln@users.noreply.github.com>
2023-08-03 09:32:12 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.write('Hello World')
+st.write('Hello World!')
 
 df = pd.read_csv('Galapagos Islands.csv')
 st.dataframe(df)
