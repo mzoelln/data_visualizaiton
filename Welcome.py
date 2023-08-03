afbfdf542eb0289cc591c5d91c9edf641da667a8
mzoelln <138578608+mzoelln@users.noreply.github.com>
2023-08-03 12:55:21 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -34,7 +34,7 @@ for i in options:
   p = figure(x_axis_label=x_axis+' (wt%)', y_axis_label=y_axis+' (wt%)', title=i[:-4])
   p.circle(data[x_axis]/10000, data[y_axis]/10000)
   p.line([data[x_axis].min()/10000, data[x_axis].max()/10000], [data[y_axis].mean()/10000,data[y_axis].mean()/10000], line_color='green' )
-  p.rect(x=df[x_axis].mean()/10000, y=df[y_axis].mean()/10000, width=df[x_axis].std()/10000, height=df[y_axis].std()/10000, color='tomato', fill_alpha=0.5)
+  p.rect(x=data[x_axis].mean()/10000, y=data[y_axis].mean()/10000, width=data[x_axis].std()/10000, height=data[y_axis].std()/10000, color='tomato', fill_alpha=0.5)
   p.line([data[x_axis].mean()/10000, data[x_axis].mean()/10000], [data[y_axis].min()/10000,data[y_axis].max()/10000], line_color='red' )
   st.bokeh_chart(p, use_container_width=True)
 
