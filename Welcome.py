682964c3b0979359c480be3e4e78f1e48124c2e0
mzoelln <138578608+mzoelln@users.noreply.github.com>
2023-08-03 09:41:19 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+
+filename_ls = []
+for i in os.listdir():
+  if i.endswith('.csv'):
+    filename_ls.append(i)
+
+st.write(filename_ls)
 
 st.write('Hello World!')
 
 df = pd.read_csv('Galapagos Islands.csv')
 st.dataframe(df)
+
+el_list = df.columns.tolist()[27:80]
+x_axis = st.selectbox('select element', el_list)
