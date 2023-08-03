21ad108ca51339fe5d5e0465341b8f368e5fe796
mzoelln <138578608+mzoelln@users.noreply.github.com>
2023-08-03 12:42:53 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -28,11 +28,13 @@ y_axis = st.selectbox('select y-axis element', el_list)
 # st.pyplot(fig)
 
 # plot with bokeh
+p = figure(x_axis_label=x_axis+' (wt%)', y_axis_label=y_axis+' (wt%)')
+
 for i in options:
   data = pd.read_csv(i)
-  p = figure(x_axis_label=x_axis+' (wt%)', y_axis_label=y_axis+' (wt%)', title=i[:-4])
   p.circle(data[x_axis]/10000, data[y_axis]/10000)
   p.line([data[x_axis].min()/10000, data[x_axis].max()/10000], [data[y_axis].mean()/10000,data[y_axis].mean()/10000], line_color='green' )
+  p.rect
   p.line([data[x_axis].mean()/10000, data[x_axis].mean()/10000], [data[y_axis].min()/10000,data[y_axis].max()/10000], line_color='red' )
 
   st.bokeh_chart(p, use_container_width=True)
