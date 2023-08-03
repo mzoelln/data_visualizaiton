cfb1dbaa11609de352e201241c2131e35bc9c104
mzoelln <138578608+mzoelln@users.noreply.github.com>
2023-08-03 09:42:58 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -8,8 +8,6 @@ for i in os.listdir():
   if i.endswith('.csv'):
     filename_ls.append(i)
 
-st.write(filename_ls)
-
 st.write('Hello World!')
 
 df = pd.read_csv('Galapagos Islands.csv')
@@ -17,3 +15,5 @@ st.dataframe(df)
 
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element', el_list)
+
+st.selectbox('select location', filename_ls)
