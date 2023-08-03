75618d6e1218aac3a8762f5fce3393cd30227ee7
mzoelln <138578608+mzoelln@users.noreply.github.com>
2023-08-03 12:28:20 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -32,6 +32,8 @@ for i in options:
   data = pd.read_csv(i)
   p = figure(x_axis_label=x_axis+' (wt%)', y_axis_label=y_axis+' (wt%)', title=i[:-4])
   p.circle(data[x_axis]/10000, data[y_axis]/10000)
-  p.hspan(y=[data[x_axis].mean()])
+  p.line([data[x_axis].min()/10000, data[x_axis].max()/10000], [data[y_axis].mean()/10000,data[y_axis].mean()/10000] )
+  p.line([data[x_axis].mean()/10000, data[x_axis].mean()/10000], [data[y_axis].min()/10000,data[y_axis].max()/10000] )
+
   st.bokeh_chart(p, use_container_width=True)
 
