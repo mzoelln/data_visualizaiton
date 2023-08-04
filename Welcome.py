c41e4a73035aeb38d87ad61525fa379eb1629932
mzoelln <138578608+mzoelln@users.noreply.github.com>
2023-08-04 15:12:32 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -12,6 +12,8 @@ for i in os.listdir():
 
 st.write('Plotting data with bokeh in Streamlit')
 
+st.radius(1,2,3)
+
 df = pd.read_csv(filename_ls[0])
 el_list = df.columns.tolist()[27:80]
 # st.dataframe(df)
@@ -37,4 +39,6 @@ for i in options:
   p.rect(x=data[x_axis].mean()/10000, y=data[y_axis].mean()/10000, width=data[x_axis].std()/10000, height=data[y_axis].std()/10000, color='blue', fill_alpha=0.25)
   p.line([data[x_axis].mean()/10000, data[x_axis].mean()/10000], [data[y_axis].min()/10000,data[y_axis].max()/10000], line_color='red' )
   st.bokeh_chart(p, use_container_width=True)
+
+
 
