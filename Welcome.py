d7d1ed1078608140f9238e3a3fa178786784e3ea
mzoelln <138578608+mzoelln@users.noreply.github.com>
2023-08-04 15:43:47 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -40,17 +40,17 @@ for i in options:
     st.write(i[:-4])
     st.dataframe(data)
     
-  with tab2:
-    p = figure(x_axis_label=x_axis+' (wt%)', y_axis_label=y_axis+' (wt%)', title=i[:-4])
-    p.circle(data[x_axis]/10000, data[y_axis]/10000)
-    p.line([data[x_axis].min()/10000, data[x_axis].max()/10000], [data[y_axis].mean()/10000,data[y_axis].mean()/10000], line_color='green' )
-    p.rect(x=data[x_axis].mean()/10000, y=data[y_axis].mean()/10000, width=data[x_axis].std()/10000, height=data[y_axis].std()/10000, color='blue', fill_alpha=0.25)
-    p.line([data[x_axis].mean()/10000, data[x_axis].mean()/10000], [data[y_axis].min()/10000,data[y_axis].max()/10000], line_color='red' )
-    st.bokeh_chart(p, use_container_width=True)
+  # with tab2:
+  #  p = figure(x_axis_label=x_axis+' (wt%)', y_axis_label=y_axis+' (wt%)', title=i[:-4])
+  #  p.circle(data[x_axis]/10000, data[y_axis]/10000)
+  #  p.line([data[x_axis].min()/10000, data[x_axis].max()/10000], [data[y_axis].mean()/10000,data[y_axis].mean()/10000], line_color='green' )
+  #  p.rect(x=data[x_axis].mean()/10000, y=data[y_axis].mean()/10000, width=data[x_axis].std()/10000, height=data[y_axis].std()/10000, color='blue', fill_alpha=0.25)
+  #  p.line([data[x_axis].mean()/10000, data[x_axis].mean()/10000], [data[y_axis].min()/10000,data[y_axis].max()/10000], line_color='red' )
+  #  st.bokeh_chart(p, use_container_width=True)
     
-  with tab3:
-    st.write('Mean of '+ str(x_axis) +' in wt%: ' + str(round(data[x_axis].mean()/10000, 2)))
-    st.write('Mean of '+ str(y_axis) +' in wt%: ' + str(round(data[y_axis].mean()/10000, 2)))
+  # with tab3:
+  #  st.write('Mean of '+ str(x_axis) +' in wt%: ' + str(round(data[x_axis].mean()/10000, 2)))
+  #  st.write('Mean of '+ str(y_axis) +' in wt%: ' + str(round(data[y_axis].mean()/10000, 2)))
 
 
 
