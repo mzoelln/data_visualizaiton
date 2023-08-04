ec2ada9215f584d657ae5511d8ed6e13a7197d9d
mzoelln <138578608+mzoelln@users.noreply.github.com>
2023-08-04 15:31:25 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -20,6 +20,8 @@ el_list = df.columns.tolist()[27:80]
 
 options = st.multiselect('select location', filename_ls)
 
+tab1, tab2 = st.tabs(['Data', 'Plot'])
+
 x_axis = st.selectbox('select x-axis element', el_list)
 y_axis = st.selectbox('select y-axis element', el_list)
 
@@ -33,12 +35,16 @@ y_axis = st.selectbox('select y-axis element', el_list)
 
 for i in options:
   data = pd.read_csv(i)
-  p = figure(x_axis_label=x_axis+' (wt%)', y_axis_label=y_axis+' (wt%)', title=i[:-4])
-  p.circle(data[x_axis]/10000, data[y_axis]/10000)
-  p.line([data[x_axis].min()/10000, data[x_axis].max()/10000], [data[y_axis].mean()/10000,data[y_axis].mean()/10000], line_color='green' )
-  p.rect(x=data[x_axis].mean()/10000, y=data[y_axis].mean()/10000, width=data[x_axis].std()/10000, height=data[y_axis].std()/10000, color='blue', fill_alpha=0.25)
-  p.line([data[x_axis].mean()/10000, data[x_axis].mean()/10000], [data[y_axis].min()/10000,data[y_axis].max()/10000], line_color='red' )
-  st.bokeh_chart(p, use_container_width=True)
+  with tab1:
+    st.write(i[:-4])
+    st.dataframe(data)
+  with tab2:
+    p = figure(x_axis_label=x_axis+' (wt%)', y_axis_label=y_axis+' (wt%)', title=i[:-4])
+    p.circle(data[x_axis]/10000, data[y_axis]/10000)
+    p.line([data[x_axis].min()/10000, data[x_axis].max()/10000], [data[y_axis].mean()/10000,data[y_axis].mean()/10000], line_color='green' )
+    p.rect(x=data[x_axis].mean()/10000, y=data[y_axis].mean()/10000, width=data[x_axis].std()/10000, height=data[y_axis].std()/10000, color='blue', fill_alpha=0.25)
+    p.line([data[x_axis].mean()/10000, data[x_axis].mean()/10000], [data[y_axis].min()/10000,data[y_axis].max()/10000], line_color='red' )
+    st.bokeh_chart(p, use_container_width=True)
 
 
 
